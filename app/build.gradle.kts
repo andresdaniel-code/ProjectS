87116e5de8b625d2b52c0b01ee1578f29122df57
andresdaniel-code <andresdanielsenaprj@gmail.com>
2026-04-16 17:40:11 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     // Plugin de Firebase (Google Services)
     // Necesario para usar servicios como Authentication, Firestore, Analytics, etc.
     id("com.google.gms.google-services")
+
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
@@ -40,6 +43,8 @@ android {
     buildFeatures {
         // Activa View Binding para acceder a las vistas de forma segura (sin findViewById)
         viewBinding = true
+        // activar Jetpack Compose en tu proyecto de Android
+        compose = true
     }
 }
 
@@ -67,16 +72,14 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // Jetpack Compose BOM (recomendado)
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    // Jetpack Compose
+    implementation(platform("androidx.compose:compose-bom:2025.12.00"))  // ← Actualizado
 
-    // Core de Compose
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.navigation:navigation-compose:2.8.4")  // para navegación futura
 
-    // Activity para Compose
     implementation("androidx.activity:activity-compose:1.9.3")
+
 }
