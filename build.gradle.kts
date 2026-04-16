01966bb0d1148e578e3c390237a6e0d81ca5bf18
andresdaniel-code <andresdanielsenaprj@gmail.com>
2026-04-16 17:40:21 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     // Plugins principales de Android y Kotlin
     alias(libs.plugins.android.application) apply false
-    //alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.android) apply false
+
+    alias(libs.plugins.compose.compiler) apply false
 
     // Plugin de Firebase Google Services
     id("com.google.gms.google-services") version "4.4.4" apply false
